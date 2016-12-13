bf2a6f282c181bcb284f69192380283827ea08ee
Usman Ayyaz <uayyaz@mit.edu>
2016-12-13 12:34:58 -0500

    merged viz conflict

--- graph_viz.js
+++ graph_viz.js
@@ -10,7 +10,7 @@
 function loadFile(time){
     $.ajax({
         type: "GET",
-        url: "http://web.mit.edu/uayyaz/www/final_output.txt",
+        url: "http://web.mit.edu/uayyaz/www/final_output.csv",
         dataType: "text",
         success: function(data) {processData(data,time);}
      });
@@ -21,6 +21,7 @@ function processData(allText,time) {
     lines = allTextLines.length
     // var headers = allTextLines[0].split('\t');
     var lines = [];
+    var twids = [];
     graph = {}
     count = 0
     for (var i=1; i<allTextLines.length/time; i++) {
@@ -39,16 +40,17 @@ function processData(allText,time) {
                 size = 0
             }
             var randomness = 0.0000//1;
-            lines.push([parseFloat(data[4]) + (Math.random()-0.5)*randomness,parseFloat(data[5])+ (Math.random()-0.5)*randomness,parseInt(data[6]),size])
+            lines.push([parseFloat(data[4]) + (Math.random()-0.5)*randomness,parseFloat(data[5])+ (Math.random()-0.5)*randomness,parseInt(data[6]),size]);
+            twids.push(data[0]);
         }
     }
     console.log(count)
-    updateGraph(lines)
+    updateGraph(lines, twids);
 
 }
 
 
-function updateGraph(lines){
+function updateGraph(lines, twids){
     google.charts.load('upcoming', {'packages': ['geochart']});
     google.charts.setOnLoadCallback(drawMarkersMap);
 
@@ -59,17 +61,34 @@ function updateGraph(lines){
         data.addColumn('number', 'Color');
         data.addColumn('number', 'Size'); 
 
-        data.addRows(lines)
+            data.addRows(lines)
 
       var options = {
         region: 'SY',
         displayMode: 'markers',
         colorAxis: {colors: ['green', 'red', "yellow", "black", "orange"],minValue: 0,maxValue:4},
         backgroundColor: "#2b8cbe",
-        sizeAxis: {minValue: 1, maxValue:4}
+        sizeAxis: {minValue: 1, maxValue:4},
       };
 
       var chart = new google.visualization.GeoChart(document.getElementById('chart_div'));
+      google.visualization.events.addListener(chart, 'select', function () {
+          var item = chart.getSelection()[0];
+          if (item) {
+              var index = item.row;
+              var tweet_box = document.getElementById('twitter_id');
+
+               $.ajax({
+                   type: "GET",
+                   data: {
+                       url: "https://twitter.com/malekbr/status/"+twids[index]
+                   },
+                   url: "https://publish.twitter.com/oembed",
+                   dataType: "json",
+                   success: function(data) {tweet_box.innerHTML = data.html;}
+                });
+          }
+      });
       chart.draw(data, options);
     };
 }
